cfe9cf18e9fc6f964abe936602cf7d88eca26537
Johoan <38538750+Johoan@users.noreply.github.com>
2021-04-29 07:22:13 +0100

    Resetting the password

---
+++ reset-password.php
@@ -0,0 +1,47 @@
+<?php 
+
+
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+
+        <title>Password Reset</title>
+
+    </head>
+
+<body>
+
+    <form method="post">
+
+        <fieldset>
+
+            <legend>Reset Password</legend>
+
+            <input type="password" name="newpassword" placeholder="enter new password"><br><br>
+            <input type="password" name="newpassword" placeholder="re-enter new password"><br><br>
+            <input type="submit" value="Reset">
+
+
+
+
+        </fieldset>
+
+
+
+    </form>
+
+
+</body>
+
+
+
+
+</html>
